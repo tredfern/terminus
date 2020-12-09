bd374448078448ecd5928db22b86a8afa2e0342b
Trevor Redfern <trevorredfern@gmail.com>
2020-12-08 21:05:56 -0500

    Removed some more dead files and implemented basic map

--- game/ui/widgets/combat_map.lua
+++ game/ui/widgets/combat_map.lua
@@ -4,9 +4,26 @@
 -- https://opensource.org/licenses/MIT
 
 local components = require "moonpie.ui.components"
+local colors = require "moonpie.graphics.colors"
+
+local tile_width = 32
+local tile_height = 32
+
+local function draw_tile(x, y)
+  love.graphics.setColor(colors.light_gray)
+  love.graphics.rectangle("fill", x * tile_width, y*tile_height, tile_width, tile_height)
+  love.graphics.setColor(colors.dark_gray)
+  love.graphics.rectangle("line", x * tile_width, y*tile_height, tile_width, tile_height)
+end
 
 return components("combat_map", function()
   return {
-
+    draw_component = function()
+      for x = 1,8 do
+        for y = 1, 8 do
+          draw_tile(x, y)
+        end
+      end
+    end
   }
 end)
