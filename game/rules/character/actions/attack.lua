1868057330ef749372b127eb7513147a652e531e
Trevor Redfern <trevorredfern@gmail.com>
2021-01-30 14:20:00 -0500

    Message log when attacks occur

--- game/rules/character/actions/attack.lua
+++ game/rules/character/actions/attack.lua
@@ -5,6 +5,7 @@
 
 local set_health = require "game.rules.character.actions.set_health"
 local types = require "game.rules.character.actions.types"
+local message_log = require "game.rules.message_log"
 
 return function(source, target)
   if source == target then return {} end
@@ -14,6 +15,12 @@ return function(source, target)
   }, {
     __call = function(_, dispatch)
       dispatch(set_health(target, target.health - 1))
+
+      -- Message Log Entry
+      local src = source.name or tostring(source)
+      local trg = target.name or tostring(target)
+      local str = src .. " attacked " .. trg
+      dispatch(message_log.actions.add(str))
     end
   })
 end
--- game/rules/character/actions/attack_spec.lua
+++ game/rules/character/actions/attack_spec.lua
@@ -24,4 +24,17 @@ describe("game.rules.character.actions.attack", function()
 
     assert.same({}, attack(source, target))
   end)
+
+  it("dispatches a message when an attack is performed", function()
+    local message_types = require "game.rules.message_log.actions.types"
+    local source, target = {}, { health = 8 }
+    local state = {
+      characters = { source, target }
+    }
+
+    local atk = attack(source, target)
+    atk(mock_dispatch, state)
+    assert.is_true(mock_dispatch:received_action(
+      message_types.message_log_add))
+  end)
 end)
