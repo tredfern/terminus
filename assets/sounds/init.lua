b89d7c0c8c08b2e4ab70ba93d38b46593b3d114a
Trevor Redfern <trevorredfern@gmail.com>
2021-03-25 22:23:02 -0400

    Closes #63 - better sound management

--- assets/sounds/init.lua
+++ assets/sounds/init.lua
@@ -4,37 +4,24 @@
 -- https://opensource.org/licenses/MIT
 
 local Audio = require "moonpie.audio"
-
-local function wrapper(soundSource)
-  return {
-    play = function()
-      soundSource:seek(0)
-      local pitchMod = 0.9 + love.math.random(0, 10)/50
-      soundSource:setPitch(pitchMod)
-      soundSource:play()
-    end,
-    setVolume = function(v)
-      soundSource:setVolume(v)
-    end
-  }
-end
+local pitchShifter = require "moonpie.audio.effects.random_pitch"
 
 local sounds = {
-  attack = wrapper(Audio.getStatic("assets/sounds/attack.wav")),
-  click = wrapper(Audio.getStatic("assets/sounds/click_sound.wav")),
-  hit = wrapper(Audio.getStatic("assets/sounds/hit.wav")),
-  hover = wrapper(Audio.getStatic("assets/sounds/hover_sound.wav")),
+  attack = Audio.getStatic("assets/sounds/attack.wav"),
+  click = pitchShifter(Audio.getStatic("assets/sounds/click_sound.wav"), 0.9, 1.3),
+  hit = Audio.getStatic("assets/sounds/hit.wav"),
+  hover = pitchShifter(Audio.getStatic("assets/sounds/hover_sound.wav"), 0.9, 1.3),
   titleTrack = Audio.getStreaming("assets/music/title.ogg"),
-  walk = wrapper(Audio.getStatic("assets/sounds/walk.wav")),
+  walk = Audio.getStatic("assets/sounds/walk.wav"),
 }
 
-sounds.attack.setVolume(0.05)
-sounds.click.setVolume(0.08)
-sounds.hit.setVolume(0.05)
-sounds.hover.setVolume(0.03)
-sounds.titleTrack:setLooping(true)
-sounds.titleTrack:setVolume(0.5)
-sounds.walk.setVolume(0.05)
+sounds.attack.source:setVolume(0.05)
+sounds.click.source:setVolume(0.08)
+sounds.hit.source:setVolume(0.05)
+sounds.hover.source:setVolume(0.03)
+sounds.titleTrack.source:setLooping(true)
+sounds.titleTrack.source:setVolume(0.5)
+sounds.walk.source:setVolume(0.05)
 
 
 return sounds
