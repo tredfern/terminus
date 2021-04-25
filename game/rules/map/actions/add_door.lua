aff65f9cd9d673b0d4898a5b40648693afac2e4e
Trevor Redfern <trevorredfern@gmail.com>
2021-04-25 12:13:38 -0400

    Only adds one door per position
    Closed #14

--- game/rules/map/actions/add_door.lua
+++ game/rules/map/actions/add_door.lua
@@ -6,8 +6,20 @@
 local addEntity = require "game.rules.world.actions.add_entity"
 local doors = require "assets.maps.features.doors"
 local Animator = require "game.rules.graphics.animator"
+local store = require "game.store"
+local World = require "game.rules.world"
+local tables = require "moonpie.tables"
+
+local function validateAction(position)
+  local entities = World.selectors.getByPosition(store.getState(), position)
+  return not tables.any(entities, function(e) return e.door end)
+end
 
 return function(position, orientation)
+  if not validateAction(position) then
+    return nil
+  end
+
   local a = Animator:new()
   a:addAnimation("opening", doors[orientation].opening)
   local entity = {
--- game/rules/map/actions/add_door_spec.lua
+++ game/rules/map/actions/add_door_spec.lua
@@ -7,6 +7,7 @@ describe("game.rules.map.actions.add_door", function()
   local addDoor = require "game.rules.map.actions.add_door"
   local Position = require "game.rules.world.position"
   local Orientation = require "game.rules.world.orientation"
+  local mockStore = require "test_helpers.mock_store"
 
   it("specifies a door entity to add", function()
     local action = addDoor(Position(2, 2, 3), Orientation.northSouth)
@@ -16,5 +17,15 @@ describe("game.rules.map.actions.add_door", function()
     assert.is_true(action.payload.entity.blocksMovement)
     assert.not_nil(action.payload.entity.sprite)
     assert.equals(Orientation.northSouth, action.payload.entity.orientation)
+  end)
+
+  it("is invalid if there is already a door at the location", function()
+    mockStore {
+      world = {
+        { position = Position(2, 2, 3), door = true }
+      }
+    }
+    local action = addDoor(Position(2, 2, 3), Orientation.northSouth)
+    assert.is_nil(action)
   end)
 end)
