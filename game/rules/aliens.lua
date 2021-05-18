dfb52f7a9a520ee8db73883c70c7fe936224bcc7
Trevor Redfern <trevorredfern@gmail.com>
2021-05-17 21:05:22 -0400

    Refactored Aliens rules area #122

---
+++ game/rules/aliens.lua
@@ -0,0 +1,53 @@
+-- Copyright (c) 2021 Trevor Redfern
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+local tables = require "moonpie.tables"
+local sprite = require "game.rules.graphics.sprite"
+local Animator = require "game.rules.graphics.animator"
+local World = require "game.rules.world"
+
+local Aliens = {
+  actions = {},
+  selectors = {}
+}
+
+local aiRoutines = {
+  require "game.rules.npcs.ai.move_towards_player",
+  require "game.rules.npcs.ai.random_movement"
+}
+
+function Aliens.actions.addAlien(position)
+  local Characters = require "game.rules.character"
+  local alienIdle = require "assets.bestiary.alien"
+
+  local alien = Characters.create { position = position, isEnemy = true, ai = tables.pickRandom(aiRoutines) }
+  alien.alien = true
+  alien.animator = Animator:new()
+  alien.animator:addAnimation("idle", alienIdle)
+  alien.animator:play("idle")
+  return Characters.actions.add(alien)
+end
+
+function Aliens.actions.addSpawner(position)
+  return World.actions.addEntity {
+    position = position,
+    spawner = true,
+    sprite = sprite.fromFile("assets/graphics/spawner.png")
+  }
+end
+
+function Aliens.actions.removeSpawner(spawner)
+  return World.actions.removeEntity(spawner)
+end
+
+function Aliens.selectors.getSpawnerAt(state, position)
+  return World.selectors.getByPosition(state, position, "spawner")[1]
+end
+
+function Aliens.selectors.getSpawners(state)
+  return World.selectors.getAllWithComponents(state, "spawner")
+end
+
+return Aliens
--- game/rules/aliens/selectors/get_spawners.lua
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2021 Trevor Redfern
---
--- This software is released under the MIT License.
--- https://opensource.org/licenses/MIT
-
-return function(state)
-  local World = require "game.rules.world"
-  return World.selectors.getAllWithComponents(state, "spawner")
-end
