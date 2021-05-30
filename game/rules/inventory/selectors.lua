b7af452cfece78f9733eb4ed1dea6432ce58f4ed
Trevor Redfern <trevorredfern@gmail.com>
2021-05-30 11:02:15 -0400

    Selectors for checking carrying items #103

--- game/rules/inventory/selectors.lua
+++ game/rules/inventory/selectors.lua
@@ -3,6 +3,19 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
+local tables = require "moonpie.tables"
+
 local selectors = {}
+
+
+function selectors.getInventory(state, entity)
+  return state.inventory[entity]
+end
+
+function selectors.isCarryingItem(state, entity, item)
+  local inv = selectors.getInventory(state, entity)
+
+  return tables.findFirst(inv.carried, function(slot) return slot.item == item end) ~= nil
+end
 
 return selectors
--- game/rules/inventory/selectors_spec.lua
+++ game/rules/inventory/selectors_spec.lua
@@ -6,4 +6,32 @@
 describe("game.rules.inventory.selectors", function()
   local selectors = require "game.rules.inventory.selectors"
 
+
+  describe("SELECTOR: getInventory", function()
+    local entity, entityInventory = {}, {}
+    local state = {
+      inventory = {
+        [entity] = entityInventory
+      }
+    }
+
+    assert.equals(entityInventory, selectors.getInventory(state, entity))
+  end)
+
+  describe("SELECTOR: isCarryingItem", function()
+    local entity, item = {}, {}
+    local entityInventory = {
+      carried = {
+        { item = item, quantity = 3 }
+      }
+    }
+    local state = {
+      inventory = {
+        [entity] = entityInventory
+      }
+    }
+
+    assert.is_true(selectors.isCarryingItem(state, entity, item))
+    assert.is_false(selectors.isCarryingItem(state, entity, {}))
+  end)
 end)
